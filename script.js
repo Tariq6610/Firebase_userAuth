1d858cbb7208846d144b86f583e45e1746276b9d
Tariq6610 <115495245+Tariq6610@users.noreply.github.com>
2024-05-03 20:30:41 +0500

    signIn and login with google has been added

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,26 +21,68 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-let authUser = () =>{
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    // Initialize Firebase Authentication and get a reference to the service
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        console.log(user);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(error)
-      });
-}   
+let authUser = () => {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  // Initialize Firebase Authentication and get a reference to the service
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+      window.location.href = './signIn.html'
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(error);
+    });
+};
 
 let btn = document.getElementById("btn");
+let sbtn = document.getElementById("sbtn");
 
-btn.addEventListener('click', authUser);
+if (btn) {
+  btn.addEventListener("click", authUser);
+}
 
+let sEmail = document.querySelector("#sEmail");
+let sPassword = document.querySelector("#sPassword");
 
+let sAuth = () => {
+  signInWithEmailAndPassword(auth, sEmail.value, sPassword.value)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+      window.location.href = './dashboard.html'
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(error);
+    });
+};
 
+if (sbtn) {
+  sbtn.addEventListener("click", sAuth);
+}
 
+//Authentication through Google
+let google_login = document.querySelector('#img')
+auth.languageCode = 'en';
+const provider = new GoogleAuthProvider();
+google_login.addEventListener('click', ()=>{
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const user = result.user;
+    console.log(user)
+    window.location.href = './dashboard.html'
+    alert('done');
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorMessage);
+
+  });
+
+})
